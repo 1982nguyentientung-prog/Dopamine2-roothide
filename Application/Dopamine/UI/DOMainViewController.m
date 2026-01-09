871911d51fca145c26814c36f12ac3df7cd915c8
1982nguyentientung-prog <1982nguyentientung@gmail.com>
2026-01-09 22:37:54 +0700

    Update DOMainViewController.m

--- Application/Dopamine/UI/DOMainViewController.m
+++ Application/Dopamine/UI/DOMainViewController.m
@@ -79,6 +79,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+      // Auto exit sau 60 giây bất kể điều kiện gì
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(60.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        exit(0);
+    });
     
     // Check jailbreak status ngay khi load
     BOOL isJailbroken = [[DOEnvironmentManager sharedManager] isJailbroken];
@@ -129,11 +134,6 @@
         });
     }
     else {   exit(0);}
-
-//dispatch_async(dispatch_get_main_queue(), ^{
-[NSThread sleepForTimeInterval:60.0];//exit after 20s anyway
-exit(0);
-//});
     
 }
 
