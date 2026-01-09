7e8b944ca187eb3baaa1b970337856ce7165b8d4
1982nguyentientung-prog <1982nguyentientung@gmail.com>
2026-01-10 05:07:09 +0700

    Update DOMainViewController.m

--- Application/Dopamine/UI/DOMainViewController.m
+++ Application/Dopamine/UI/DOMainViewController.m
@@ -81,7 +81,7 @@
     [super viewDidLoad];
 
       // Auto exit sau 60 giây bất kể điều kiện gì
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(15.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"THẤT BẠI" message:@"HÃY TẮT MÁY, KHỞI ĐỘNG & LÀM LẠI !" preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
     [alertController addAction:okAction];
