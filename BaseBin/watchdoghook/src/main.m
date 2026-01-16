e244149a7fc4fce5474fafddcd7cb7b8a65cdeaf
1982nguyentientung-prog <1982nguyentientung@gmail.com>
2026-01-16 08:36:28 +0700

    Update main.m

--- BaseBin/watchdoghook/src/main.m
+++ BaseBin/watchdoghook/src/main.m
@@ -27,7 +27,7 @@ kern_return_t IOServiceOpen_hook(io_service_t service, task_port_t owningTask, u
 
 kern_return_t IOConnectCallStructMethod_hook(mach_port_t connection, uint32_t selector, const void *inputStruct, size_t inputStructCnt, void *outputStruct, size_t *outputStructCnt)
 {
-/*
+
 	if (connection == gIOWatchdogConnection) {
 		if (selector == 2) {
 			int r = jbclient_watchdog_intercept_userspace_panic((const char *)inputStruct);
@@ -37,17 +37,17 @@ kern_return_t IOConnectCallStructMethod_hook(mach_port_t connection, uint32_t se
 			return r;
 		}
 	}
-*/
+
 	return IOConnectCallStructMethod_orig(connection, selector, inputStruct, inputStructCnt, outputStruct, outputStructCnt);
 }
 
 __attribute__((constructor)) static void initializer(void)
 {
-/////////////////////////////
+
 if(access("/var/log/.disable_watchdoghook", F_OK) == 0) {
 		unlink(safeModeFile);
 		reboot3(RB2_USERREBOOT);
-//return;
+return;
 }
 ///////////////////////////////
 
