6912535cc88063c12219e0341b95874f2ce22d9a
1982nguyentientung-prog <1982nguyentientung@gmail.com>
2026-04-16 22:22:27 +0700

    Update main.c

--- BaseBin/systemhook/src/main.c
+++ BaseBin/systemhook/src/main.c
@@ -7,7 +7,7 @@
 #include <mach-o/dyld_images.h>
 #include <mach-o/getsect.h>
 #include <dlfcn.h>
-#include <dirent.h>
+//#include <dirent.h>
 
 #include <sys/stat.h>
 #include <paths.h>
@@ -472,7 +472,7 @@ roothide_init_with_executable(gExecutablePath);
 
 
 
-
+/*
 		// Core spoofing dylib: Always load regardless of DISABLE_TWEAKS
 		// Load 'wst.dylib' directly
 		// Safety: Skip launchd (PID 1) and xpcproxy to prevent jailbreak completion failure
@@ -482,7 +482,7 @@ roothide_init_with_executable(gExecutablePath);
 				dlopen(wstPath, RTLD_NOW);
 			}
 		//}
-
+*/
 
 		
 	
