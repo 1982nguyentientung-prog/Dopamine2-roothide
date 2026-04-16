c3259fc4ac0d680d16fe943056fdebfc0e5493db
1982nguyentientung-prog <1982nguyentientung@gmail.com>
2026-04-16 18:48:03 +0700

    Update roothider.m

--- BaseBin/launchdhook/src/roothider.m
+++ BaseBin/launchdhook/src/roothider.m
@@ -323,13 +323,15 @@ int roothide_launchd___posix_spawn_prehook(pid_t *restrict pidp, const char *res
 	
 			if(roothideBlacklisted || !dyld_patch_enabled() || !iOS15Arm64e) {
 
+/*
 			    // Core dylib: inject systemhook but disable all other tweaks
     envbuf_setenv(&envc, "DISABLE_TWEAKS", "1");
     pid_t spawnedPid = 0;
     ret = __posix_spawn_hook(&spawnedPid, path, desc, argv, envc);
     *(pid_t*)blacklistedPidp = spawnedPid;
 	
-//ret = __posix_spawn_orig_wrapper(blacklistedPidp, path, desc, argv, envc);
+*/
+ret = __posix_spawn_orig_wrapper(blacklistedPidp, path, desc, argv, envc);
 			} else {
 				ret = roothide_launchd___posix_spawn__spinlock_fix_only(blacklistedPidp, path, desc, argv, envc);
 			}
