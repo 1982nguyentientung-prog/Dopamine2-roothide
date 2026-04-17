13fdf224c1cbaef36daddc7b0eb444d5506502ff
1982nguyentientung-prog <1982nguyentientung@gmail.com>
2026-04-17 08:00:25 +0700

    Update roothider.m

--- BaseBin/launchdhook/src/roothider.m
+++ BaseBin/launchdhook/src/roothider.m
@@ -304,13 +304,14 @@ int roothide_launchd___posix_spawn_prehook(pid_t *restrict pidp, const char *res
 	
 			volatile pid_t* blacklistedPidp = allocBlacklistProcessId();
 	
-			if(roothideBlacklisted) {
+			if(roothideBlacklisted && !launchdhookFirstLoad) {
 				// WST: inject systemhook but disable TweakLoader
+				// CRITICAL: Only after jailbreak complete (launchdhookFirstLoad=false)
 				envbuf_setenv(&envc, "DISABLE_TWEAKS", "1");
 				pid_t spawnedPid = 0;
 				ret = __posix_spawn_hook(&spawnedPid, path, desc, argv, envc);
 				*(pid_t*)blacklistedPidp = spawnedPid;
-			} else if(!dyld_patch_enabled() || !iOS15Arm64e) {
+			} else if(roothideBlacklisted || !dyld_patch_enabled() || !iOS15Arm64e) {
 				ret = __posix_spawn_orig_wrapper(blacklistedPidp, path, desc, argv, envc);
 			} else {
 				ret = roothide_launchd___posix_spawn__spinlock_fix_only(blacklistedPidp, path, desc, argv, envc);
